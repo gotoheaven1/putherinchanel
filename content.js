c05d575b34bea4d093ff11288e3c4df1d4dcba60
gotoheaven1 <ansoyeolansoyeol@gmail.com>
2025-12-26 08:41:01 +0900

    Create content.js

---
+++ content.js
@@ -0,0 +1,45 @@
+export const portfolioContent = {
+    'school': {
+        title: '학교 생활 & 학업',
+        desc: [
+            '• OO고등학교 3학년 재학 중',
+            '• 컴퓨터 동아리 부장 (2023)',
+            '• 교내 알고리즘 대회 금상 수상',
+            '• 평균 학점 4.0 유지'
+        ]
+    },
+    'projects': {
+        title: '개발 프로젝트',
+        desc: [
+            '1. 나만의 포트폴리오 게임 (Phaser.js)',
+            ' - 인터랙티브 웹 포트폴리오',
+            '',
+            '2. 급식 알리미 봇 (Python)',
+            ' - Discord API 활용',
+            '',
+            '3. 투두 리스트 (React)',
+            ' - 일정 관리 웹앱'
+        ]
+    },
+    'skills': {
+        title: '기술 스택',
+        desc: [
+            'Frontend:',
+            ' - HTML5, CSS3, JavaScript',
+            ' - React, Phaser',
+            '',
+            'Backend:',
+            ' - Node.js, Python'
+        ]
+    },
+    'contact': {
+        title: '연락처 & 링크',
+        desc: [
+            '📧 Email: student@school.edu',
+            '🐙 Github: github.com/username',
+            '📝 Blog: velog.io/@username',
+            '',
+            '편하게 연락 주세요!'
+        ]
+    }
+};
